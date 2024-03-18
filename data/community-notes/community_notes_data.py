d766f49a0a01a23f8fb418db7fd9fe7b68608490
baris-yazici <barisbarisyazici@gmail.com>
2024-03-18 11:40:12 +0000

    Community notes data filter fixed.

--- data/community-notes/community_notes_data.py
+++ data/community-notes/community_notes_data.py
@@ -4,47 +4,15 @@
 
 import pandas as pd
 
-# Read the notes file
+# Read the notes and notes status files
 notes = pd.read_csv('/path/notes-00000.tsv', delimiter='\t')
+notes_status = pd.read_csv('/path/noteStatusHistory-00000.tsv', delimiter='\t')
 
-# Initialize an empty list to store helpful note IDs
-all_helpful_note_ids = []
+# Check how many are helpful vs not helpful
+notes_status["currentStatus"].value_counts()
 
-# Loop through ratings files and aggregate helpful note IDs
-for i in range(8):
-    ratings = pd.read_csv(f'/Users/barisyazici/Desktop/Community Notes Data/ratings-0000{i}.tsv', delimiter='\t')
-    helpful_note_ids = ratings.loc[ratings["helpfulnessLevel"]=="HELPFUL", "noteId"].unique()
-    all_helpful_note_ids.extend(helpful_note_ids)
-
-# Convert the list to a set to remove duplicates, then back to a list
-all_helpful_note_ids = list(set(all_helpful_note_ids))
-
-# Export the list of note ids
-all_helpful_note_ids = pd.DataFrame(all_helpful_note_ids)
-all_helpful_note_ids.columns = ["noteId"]
-#all_helpful_note_ids.to_csv("/path/helpful_note_ids.csv")
-
-# Filter and export helpful notes
-helpful_notes = notes[notes["noteId"].isin(all_helpful_note_ids["noteId"])]
-#helpful_notes.to_csv("/path/helpful_notes.csv")
-
-# Initialize an empty list to store not helpful note IDs
-all_not_helpful_note_ids = []
-
-# Loop through ratings files and aggregate not helpful note IDs
-for i in range(8):
-    ratings = pd.read_csv(f'/Users/barisyazici/Desktop/Community Notes Data/ratings-0000{i}.tsv', delimiter='\t')
-    not_helpful_note_ids = ratings.loc[ratings["helpfulnessLevel"]=="NOT_HELPFUL", "noteId"].unique()
-    all_not_helpful_note_ids.extend(not_helpful_note_ids)
-
-# Convert the list to a set to remove duplicates, then back to a list
-all_not_helpful_note_ids = list(set(all_not_helpful_note_ids))
-
-# Export the list of note ids
-all_not_helpful_note_ids = pd.DataFrame(all_helpful_note_ids)
-all_not_helpful_note_ids.columns = ["noteId"]
-#all_not_helpful_note_ids.to_csv("/path/not_helpful_note_ids.csv")
-
-# Filter and export not helpful notes
-not_helpful_notes = notes[notes["noteId"].isin(all_not_helpful_note_ids["noteId"])]
-#not_helpful_notes.to_csv("/path/not_helpful_notes.csv")
+# Filter the notes data based on the labels
+helpful_note_ids = notes_status.loc[notes_status["currentStatus"] == "CURRENTLY_RATED_HELPFUL", "noteId"].to_list()
+not_helpful_note_ids = notes_status.loc[notes_status["currentStatus"] == "CURRENTLY_RATED_NOT_HELPFUL", "noteId"].to_list()
+helpful_notes = notes[notes["noteId"].isin(helpful_note_ids)]
+not_helpful_notes = notes[notes["noteId"].isin(not_helpful_note_ids)]
